0878737239b76a22ac32933db0a9ee318cb2aa66
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:55 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Enumerable.Async.Tests/AsyncEnumerableExtensionTests.cs
+++ test/Soenneker.Extensions.Enumerable.Async.Tests/AsyncEnumerableExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.Enumerable.Async.Tests;
 
 public class AsyncEnumerableExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
